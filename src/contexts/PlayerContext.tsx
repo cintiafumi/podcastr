5fd7717cbff0f82de5aa1176cd28802b7e4b4c90
Cíntia Fumi <cintiafumi@gmail.com>
2021-05-02 21:00:37 -0300

    ♻️  refac: change context to usePlayer

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from 'react';
+import { createContext, ReactNode, useContext, useState } from 'react';
 
 type Episode = {
   title: string;
@@ -90,3 +90,5 @@ export function PlayerContextProvider({
     </PlayerContext.Provider>
   );
 }
+
+export const usePlayer = () => useContext(PlayerContext);
