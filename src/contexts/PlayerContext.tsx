f282450e8a790e4ce51c625357fc146780a00e48
Cíntia Fumi <cintiafumi@gmail.com>
2021-05-02 21:50:07 -0300

    ✨  feat: add slider effect

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -23,6 +23,7 @@ type PlayerContextData = {
   togglePlay: () => void;
   toggleLoop: () => void;
   toggleShuffle: () => void;
+  clearPlayerState: () => void;
   setPlayingState: (state: boolean) => void;
 };
 
@@ -69,8 +70,13 @@ export function PlayerContextProvider({
     setIsPlaying(state);
   }
 
+  function clearPlayerState() {
+    setEpisodeList([]);
+    setCurrentEpisodeIndex(0);
+  }
+
   const hasPrevious = currentEpisodeIndex > 0;
-  const hasNext = currentEpisodeIndex + 1 < episodeList.length;
+  const hasNext = isShuffling || currentEpisodeIndex + 1 < episodeList.length;
 
   function playNext() {
     if (isShuffling) {
@@ -107,6 +113,7 @@ export function PlayerContextProvider({
         setPlayingState,
         hasNext,
         hasPrevious,
+        clearPlayerState,
       }}
     >
       {children}
